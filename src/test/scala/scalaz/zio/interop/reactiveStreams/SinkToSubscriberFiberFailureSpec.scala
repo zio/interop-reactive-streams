ddf02ab1f7133d65c49b9b085e832b27a9d3f130
Simon Schenk <simon.schenk@risk42.com>
2020-02-03 21:31:45 +0100

    directly extend AbstractRunnableSpec

--- src/test/scala/scalaz/zio/interop/reactiveStreams/SinkToSubscriberFiberFailureSpec.scala
+++ src/test/scala/scalaz/zio/interop/reactiveStreams/SinkToSubscriberFiberFailureSpec.scala
@@ -4,35 +4,38 @@ import org.reactivestreams.{ Publisher, Subscriber, Subscription }
 import zio.internal.PlatformLive
 import zio.test._
 import zio.test.Assertion._
-import zio.test.environment.TestEnvironment
+import zio.ZManaged
 
-object SinkToSubscriberFiberFailureSpecUtil {
+object SinkToSubscriberFiberFailureSpec extends AbstractRunnableSpec {
+
+  type Environment = Unit
+  type Label       = String
+  type Test        = Any
+  type Failure     = Any
+  type Success     = Any
 
   var fibersFailed = 0
 
-  object Runner
-      extends TestRunner[TestEnvironment, String, Any, Any, Any](
-        TestExecutor.managed(TestEnvironment.Value),
-        PlatformLive.makeDefault().withReportFailure(_ => fibersFailed += 1)
-      ) {}
+  val runner: TestRunner[Environment, Label, Test, Failure, Success] =
+    new TestRunner[Environment, Label, Any, Failure, Success](
+      TestExecutor.managed[Environment, Test, Label, Success](ZManaged.unit),
+      PlatformLive.makeDefault().withReportFailure(_ => fibersFailed += 1)
+    ) {}
 
-}
-
-object SinkToSubscriberFiberFailureSpec
-    extends RunnableSpec(SinkToSubscriberFiberFailureSpecUtil.Runner)(
-      testM("Does not fail a fiber on failing Publisher") {
-        val publisher = new Publisher[Int] {
-          override def subscribe(s: Subscriber[_ >: Int]): Unit =
-            s.onSubscribe(
-              new Subscription {
-                override def request(n: Long): Unit = s.onError(new Throwable("boom!"))
-                override def cancel(): Unit         = ()
-              }
-            )
-        }
-        for {
-          exit <- publisher.toStream().runDrain.run
-        } yield assert(exit, fails(anything)) &&
-          assert(SinkToSubscriberFiberFailureSpecUtil.fibersFailed, equalTo(0))
+  val spec: ZSpec[Environment, Failure, Label, Test] =
+    testM("Does not fail a fiber on failing Publisher") {
+      val publisher = new Publisher[Int] {
+        override def subscribe(s: Subscriber[_ >: Int]): Unit =
+          s.onSubscribe(
+            new Subscription {
+              override def request(n: Long): Unit = s.onError(new Throwable("boom!"))
+              override def cancel(): Unit         = ()
+            }
+          )
       }
-    )
+      for {
+        exit <- publisher.toStream().runDrain.run
+      } yield assert(exit, fails(anything)) &&
+        assert(fibersFailed, equalTo(0))
+    }
+}
