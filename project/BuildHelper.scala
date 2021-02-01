c74e193a8c96567fa67d2d586c1462ffdf4f5d17
Scala Steward <me@scala-steward.org>
2021-02-01 15:33:14 +0100

    Update silencer-lib_2.13.3 to 1.7.2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -13,7 +13,7 @@ object BuildHelper {
   val Scala212        = "2.12.12"
   val Scala213        = "2.13.3"
   val ScalaDotty      = "0.27.0-RC1"
-  val SilencerVersion = "1.7.1"
+  val SilencerVersion = "1.7.2"
 
   private val stdOptions = Seq(
     "-deprecation",
