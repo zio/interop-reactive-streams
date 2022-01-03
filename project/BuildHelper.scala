afec02896b3a7a9c4ed003bc293552d93ce8c2d1
Simon Schenk <simon.schenk@ziverge.com>
2022-01-03 20:28:04 +0100

    Bump ZIO to 1.0.13, Scala to 2.13.6, 3.1.0

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -10,8 +10,8 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
   val Scala211   = "2.11.12"
   val Scala212   = "2.12.15"
-  val Scala213   = "2.13.6"
-  val ScalaDotty = "3.0.2"
+  val Scala213   = "2.13.7"
+  val ScalaDotty = "3.1.0"
 
   private val stdOptions = Seq(
     "-deprecation",
