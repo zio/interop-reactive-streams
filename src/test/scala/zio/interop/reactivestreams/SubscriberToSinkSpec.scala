f000bd332f489a81004f0c4b66212cfeae83b4fa
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-03-18 22:33:00 +0100

    Warnings as errors again

--- src/test/scala/zio/interop/reactivestreams/SubscriberToSinkSpec.scala
+++ src/test/scala/zio/interop/reactivestreams/SubscriberToSinkSpec.scala
@@ -15,7 +15,7 @@ object SubscriberToSinkSpec extends DefaultRunnableSpec {
     suite("Converting a `Subscriber` to a `Sink`")(
       test("works on the happy path") {
         makeSubscriber.flatMap(probe =>
-          ZIO.scoped {
+          ZIO.scoped[Any] {
             probe.underlying
               .toSink[Throwable]
               .flatMap { case (_, sink) =>
@@ -32,7 +32,7 @@ object SubscriberToSinkSpec extends DefaultRunnableSpec {
       },
       test("transports errors") {
         makeSubscriber.flatMap(probe =>
-          ZIO.scoped {
+          ZIO.scoped[Any] {
             probe.underlying
               .toSink[Throwable]
               .flatMap { case (signalError, sink) =>
@@ -49,7 +49,7 @@ object SubscriberToSinkSpec extends DefaultRunnableSpec {
       },
       test("transports errors 2") {
         makeSubscriber.flatMap(probe =>
-          ZIO.scoped {
+          ZIO.scoped[Any] {
             probe.underlying
               .toSink[Throwable]
               .flatMap { case (signalError, sink) =>
