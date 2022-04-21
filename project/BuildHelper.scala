d327ca61bee6cf3bf16d81f451e9f2c3e0dffb1c
Simon Schenk <simon.schenk@ziverge.com>
2022-04-21 13:14:39 +0200

    scala and plugin version bumps

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -10,8 +10,8 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 object BuildHelper {
   val Scala211   = "2.11.12"
   val Scala212   = "2.12.15"
-  val Scala213   = "2.13.6"
-  val ScalaDotty = "3.1.0"
+  val Scala213   = "2.13.8"
+  val ScalaDotty = "3.1.2"
 
   private val stdOptions = Seq(
     "-deprecation",
