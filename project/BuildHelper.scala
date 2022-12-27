81213ad5fbfa03b16a48d2cfa9836e57f7e89323
Milad Khajavi <khajavi@gmail.com>
2022-12-27 21:19:52 +0330

    update scala versions.

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -9,9 +9,9 @@ import scalafix.sbt.ScalafixPlugin.autoImport._
 
 object BuildHelper {
   val Scala211   = "2.11.12"
-  val Scala212   = "2.12.15"
-  val Scala213   = "2.13.8"
-  val ScalaDotty = "3.1.0"
+  val Scala212   = "2.12.17"
+  val Scala213   = "2.13.10"
+  val ScalaDotty = "3.2.1"
 
   private val stdOptions = Seq(
     "-deprecation",
